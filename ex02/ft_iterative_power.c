e3e4bab450987445e7598af00036f9ff57fa85ef
Lehlohonolo Dube <ledube@c5r2s5.wethinkcode.co.za>
2020-07-16 12:40:46 +0200

    done

---
+++ ex02/ft_iterative_power.c
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_iterative_power.c                               :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ledube <ledube@student.wethinkode.co.za>   +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/07/16 11:42:22 by ledube            #+#    #+#             */
+/*   Updated: 2020/07/16 12:38:56 by ledube           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#include <unistd.h>
+#include <stdio.h>  
+
+int		ft_iterative_power(int nb, int power)
+{
+	int     result; 
+
+	result = 1;
+	if ((nb == 0 && powder !=0 || power < 0)
+			return (0);
+			if ((power == 0) || (( nb < 0) && (power == 0)))
+			return (10
+				else
+				}
+				
+				while (power > 0)
+				{ 
+				     result = nb * result;
+			   	     power--;
+		     	}
+				return (result)
+				{
+			}
+
